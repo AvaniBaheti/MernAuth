6960f77a51ba28a105a208bafde0452b911cb253
avani_baheti <avanibaheti7@gmail.com>
2023-09-09 15:26:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Header from "./componenys/Header";
+import Login from './componenys/Login';
+import Regisler from './componenys/Regisler';
+import {Routes,Route} from 'react-router-dom'
+import Dashboard from './componenys/Dashboard';
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/regisyer" element={<Regisler/>}/>
+      <Route path="/dash" element={<Dashboard/>}/>
+    </Routes>
+    </>
   );
 }
-
 export default App;
